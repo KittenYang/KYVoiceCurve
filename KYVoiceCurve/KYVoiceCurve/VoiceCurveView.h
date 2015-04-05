72062d7c6126cce6098a13b575d10fb12eb9d15b
KittenYang <kittenyang@icloud.com>
2015-04-06 02:08:06 +0800

    updated

---
+++ KYVoiceCurve/KYVoiceCurve/VoiceCurveView.h
@@ -0,0 +1,13 @@
+//
+//  VoiceCurveView.h
+//  KYVoiceCurve
+//
+//  Created by Kitten Yang on 4/6/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoiceCurveView : UIView
+
+@end
---
+++ KYVoiceCurve/KYVoiceCurve/VoiceCurveView.m
@@ -0,0 +1,21 @@
+//
+//  VoiceCurveView.m
+//  KYVoiceCurve
+//
+//  Created by Kitten Yang on 4/6/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "VoiceCurveView.h"
+
+@implementation VoiceCurveView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
